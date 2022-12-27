149b804e730c088e96992a001ee2216b3a55e89c
Jerusha-John <115393477+Jerusha-John@users.noreply.github.com>
2022-12-27 22:50:56 +0530

    Java program to find the square root of a number.

---
+++ sqrt.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+public class Sqrt
+{
+  public static void main(String args[])
+  {
+     double a,b;
+     a=10;
+     b=Math.sqrt(a);
+     System.out.println("The sqrt of "+a+ "is "+b);
+  }
+}
