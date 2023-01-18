759652125a390132e6618fe71800f8675072c924
Jerusha-John <115393477+Jerusha-John@users.noreply.github.com>
2023-01-18 16:09:54 +0530

    File creation using File.createNewFile() method

---
+++ FCMeth1.java
@@ -0,0 +1,22 @@
+//file creation using File.createNewFile() method.
+import java.io.*;
+class FCMeth1
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			File f1=new File("D:\\java\\test12.txt");
+			boolean fc=f1.createNewFile();
+			if(fc)
+			{
+				System.out.println("File created");
+			}
+			else
+			{
+				System.out.println("File not created");
+			}
+		}
+		catch(IOException e){System.out.println(e);}
+	}
+}
