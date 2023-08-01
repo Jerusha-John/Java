701f986f2bce0c846e738363009a0b9bb8803ff9
Jerusha-John <115393477+Jerusha-John@users.noreply.github.com>
2023-08-01 22:11:29 +0530

    Runnable thread

---
+++ ThrRunnable.java
@@ -0,0 +1,30 @@
+//Write a program to implement threads by implementing the Runnable interface.
+
+class CCC implements Runnable{
+	String nm;
+	CCC(String name){
+		nm=name;
+	}
+	public void run() {
+		System.out.println("Running "+nm);
+		try{
+			for(int i=5;i>=0;i--) {
+				System.out.println(nm+ " "+i);
+				Thread.sleep(1000);
+			}
+		}
+		catch(InterruptedException e) {
+			System.out.println(e);
+		}
+	}
+}
+public class ThrRunnable {
+
+	public static void main(String[] args) {
+		CCC c=new CCC("C");
+		Thread t=new Thread(c);
+		t.start();
+
+	}
+
+}
